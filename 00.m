569ccffa83a64c15d877265399c8d54dfcda8e7c
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-08-23 17:02:53 +0200

    requirement_checks

--- 00.m
+++ 00.m
@@ -1,5 +1,5 @@
 push 21
-push 2
+push 20
 push 10
 push 100
 pall
--- push.c
+++ push.c
@@ -7,6 +7,35 @@
 */
 void _push(stack_t **head, unsigned int counter)
 {
+	int is_digit = 1, i = 0;
+
+	if (app_data.arg)
+	{
+		while (app_data.arg[i] != '\0')
+		{
+			/* check if the character is not a digit (0-9) */
+			if (app_data.arg[i] > 57 || app_data.arg[i] < 48)
+				is_digit = 0;
+			if (!is_digit)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", counter);
+				fclose(app_data.file);
+				free(app_data.line);
+				free_stack(*head);
+				exit(EXIT_FAILURE);
+			}
+			i++;
+		}
+	}
+	else
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(app_data.file);
+		free(app_data.line);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
 	if (app_data.type == STACK)
 		add_at_head(head, atoi(app_data.arg));
 	else
